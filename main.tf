71a7da8c8dd28c81faa040e4d11dddf0275bd5af
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2023-09-29 14:44:29 +0000

    #9 Feature: create aws s3 bucket using random's provider random_string

--- main.tf
+++ main.tf
@@ -1,1 +1,39 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
 
+provider "random" {
+  # Configuration options
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length  = 36
+  special = false
+  lower   = true
+  upper   = false
+}
+
+resource "aws_s3_bucket" "bucket_name" {
+  bucket = random_string.bucket_name.id
+  tags = {
+    Name = "Terraform Bootcamp Bucket"
+    Type = "tutorial"
+  }
+}
+
+output "bucket_name" {
+  value = random_string.bucket_name.id
+}
